b0d7ca0c0f008652fcd806e6b8a8f82ef1b4aa40
Ryan Alves <Ryan.queen246@gmail.com>
2024-04-09 11:22:57 -0300

    iniciando o projeto
    iniciando o front-end do projeto

---
+++ Apps/Screens/Test1.jsx
@@ -0,0 +1,10 @@
+import { View, Text ,ScrollView ,RefreshControl } from 'react-native'
+
+export default function Test1() {
+  return (
+    <ScrollView>
+      <Text> PAGINA11</Text>
+    </ScrollView>
+  )
+    
+}
---
+++ Apps/Screens/Test2.jsx
@@ -0,0 +1,11 @@
+import { View, Text ,ScrollView ,RefreshControl } from 'react-native'
+
+
+export default function Test2() {
+  return (
+    <ScrollView>
+      <Text> PAGINA2</Text>
+    </ScrollView>
+  )
+    
+}
