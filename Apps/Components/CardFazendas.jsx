3b82a05eba7ff4e21f1d67a1ee69a19efb3977c4
Lucas Vinicius <100610137+LucasVinicius32@users.noreply.github.com>
2024-04-11 00:30:47 -0300

    Add segunda Box
    Dentro do CardFazenda

--- Apps/Components/CardFazendas.jsx
+++ Apps/Components/CardFazendas.jsx
@@ -1,24 +1,34 @@
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity ,Image } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 import { Ionicons } from '@expo/vector-icons';
 import Agro from '../Components/Agro.jpeg';
-
+import Agro2 from '../Components/Agro2.jpeg';
 
 export default function Fazendas({title, number, praga}){
     const navigation = useNavigation();
 
     return (
-        <View style={styles.BoxFazendaImagem}>
-        <Image source={Agro} style={styles.image} />
-        <View style={styles.textContainer}>
-          <Text style={styles.boxText}>{title} </Text>
-          <Text style={styles.boxText}>{number}</Text>
-          <Text style={styles.boxText}>{praga}</Text>
-        </View>
-      </View>
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+            <View style={styles.BoxFazendaImagem}>
+                <Image source={Agro} style={styles.image} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.boxText}>{title}</Text>
+                    <Text style={styles.boxText}>{number}</Text>
+                    <Text style={styles.boxText}>{praga}</Text>
+                </View>
+            </View>
+            <View style={styles.BoxFazendaImagem2}>
+                <Image source={Agro2} style={styles.image} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.boxText}>Titulo da Fazenda 2</Text>
+                    <Text style={styles.boxText}>Numero Armadilhas 2</Text>
+                    <Text style={styles.boxText}>Pragas localizadas 2</Text>
+                </View>
+            </View>
+        </ScrollView>
     )
-
 }
+
 const styles = StyleSheet.create({
     BoxFazendaImagem: {
         width: 370,
@@ -29,17 +39,27 @@ const styles = StyleSheet.create({
         flexDirection: 'row', 
         alignItems: 'center', 
         padding: 10,
-     },
-     image: {
+    },
+    BoxFazendaImagem2: {
+        width: 370,
+        height: 200, 
+        backgroundColor: '#E4E4E4',
+        borderRadius: 10,
+        margin: 10,
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        padding: 10,
+        marginBottom: 60, 
+    },
+    image: {
         width: 170, 
         height: 150, 
         marginRight: 10, 
-     },
-     textContainer: {
+    },
+    textContainer: {
         flex: 1, 
         justifyContent: 'center', 
         alignItems: 'flex-start', 
         gap: 20,
-     },
-
-})
+    },
+});
