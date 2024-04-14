69c2c42b6ca960b372877df4b2a43b3ff220b85f
LucasMedici <lucasrpmedici@gmail.com>
2024-04-14 18:43:59 -0300

    descricao baseURL

--- Apps/Services/Axios.js
+++ Apps/Services/Axios.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://192.168.0.4:3000',
+
+  /// ESSE BASE URL É FUNCIONAL PRA QUEM TA USANDO O EXPO PELO CELULAR, PELO EMULADOR DO ANDROID ESTUDIO DEVE SER OUTRO, N TENHO CRTZ, MAS A PORTA É A MESMA!
 });
 
 export default api;
