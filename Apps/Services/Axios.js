9fad823ad997c7c07b86ebb12244d47f653b89f7
guilhermedcdias <guilherme.dias32@fatec.sp.gov.br>
2024-05-06 09:45:57 -0300

    cadastro de armadilhas

--- Apps/Services/Axios.js
+++ Apps/Services/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://192.168.18.16:3000',
+  baseURL: 'http://192.168.28.72:3000',
 });
 
 api.interceptors.request.use(config => {
